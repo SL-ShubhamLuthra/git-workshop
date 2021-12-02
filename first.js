b9caa85b6a46ec922b6ddbe6e3f626e9a7018cbc
ShubhamLuthtra <shubh.luthra20@bvicam.in>
2021-12-02 15:53:29 +0530

    module updated

--- first.js
+++
@@ -1,9 +0,0 @@
-alert("Hello world");
-<<<<<<< HEAD
-console.log('hey i m in development branch');
-=======
-console.log('hey i m in new-feature branch');
-console.log('hey i m in new-feature branch');
-console.log('hey i m in new-feature branch');
-console.log('hey i m in new-feature branch');
->>>>>>> ef6fb269497937e913eabb1771947d33d3e40414
